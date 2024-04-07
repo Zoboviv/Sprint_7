781b5f6b1fe9af7a15ee731004c7d35f1bfe9dcb
Igor Zobov <zobov.i.v@yandex.ru>
2024-04-07 16:22:13 +0300

    коммит 4

--- src/test/java/OrderListTest.java
+++ src/test/java/OrderListTest.java
@@ -1,5 +1,6 @@
 import client.ScooterServiceClient;
 import client.ScooterServiceClientImple;
+import io.qameta.allure.Description;
 import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
@@ -8,10 +9,11 @@ import static org.hamcrest.Matchers.notNullValue;
 public class OrderListTest {
     private ScooterServiceClient client;
     @Test
+    @Description("Проверка - в тело ответа возвращается список заказов.")
     public void createOrders(){
         client = new ScooterServiceClientImple();
         ValidatableResponse response = client.orderList();
         response.assertThat().statusCode(200).and().body("orders", notNullValue());
-
     }
+
 }
